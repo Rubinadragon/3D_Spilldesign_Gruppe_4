e41efd372fbc6250126f5cfd4fa7e7f01dde859e
Rubinadragon <cecilive@hiof.no>
2025-02-18 19:54:28 +0100

    Kommentarer til senere

--- Assets/Scripts/CrystalDestroy.cs
+++ Assets/Scripts/CrystalDestroy.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class CrystalDestroy : MonoBehaviour
 {
+    //Hente fram riftkrystaller
+    //Finne dynamisk antall krystaller basert på scenen
+    //Når spilleren rører bort i en krystall, ødelegg krystall og reduser total mengde krystaller i scenen med 1
+    //Hvis spilleren har samlet nok krystaller, spawn en siste stor riftkrystall
     public GameObject RiftCrystals;
     private int CrystalAmountInLvl;
     
